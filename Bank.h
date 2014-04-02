5a67ddc99f98a01040349d58eff2156119397a7d
adwisaty4 <a.dwisaty4@yahoo.com>
2014-04-02 18:05:57 +0700

    Finalisasi Komentar

--- Bank.h
+++ Bank.h
@@ -8,25 +8,119 @@ using namespace std;
 
 class Bank{
 public:
+	/**
+	 * @brief Constructor
+	 * @details Inisiasi objek bank dengan nilai awal
+	 */
     Bank();
-	Bank(DateTime, int);
+    /**
+     * @brief Constructor parameter
+     * @details Inisiasi objek bank dengan nilai sesuai masukan dari pengguna
+     * 
+     * @param datetime Nilai masukan tanggal dan waktu dari pengguna
+     * @param _N Jumlah teller/baris antrian yang akan dibuka
+     */
+	Bank(DateTime datetime, int _N);
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * 
+	 * @param  [description]
+	 */
 	Bank(const Bank&);
-	Bank& operator=(const Bank&);
+	/**
+	 * @brief Operator overloading=
+	 * @details Menghasilkan objek bank baru dengan nilai yang sama dengan objek bank lama
+	 * 
+	 * @param bank Objek hasil operasi=
+	 */
+	Bank& operator=(const Bank& bank);
+	/**
+	 * @brief Destructor
+	 * @details Menghapus objek bank dari memory
+	 */
 	~Bank();
-
+	/**
+	 * @brief setN
+	 * @details Mengatur jumlah barisan antrian yang boleh dibuka
+	 * 
+	 * @param jumlah barisan antrian sesuai dengan keinginan pengguna
+	 */
 	void setN(int);
+	/**
+	 * @brief setDateTime
+	 * @details Mengatur datetime dari sebuah objek bank
+	 * 
+	 * @param  masukan memiliki format tanggal-bulan-tahun;jam-menit-detik
+	 */
 	void setDateTime(DateTime);
+	/**
+	 * @brief setQueue
+	 * @details Mengatur antrian ke i dengan queue tertentu
+	 * 
+	 * @param queue Queue masukan
+	 * @param i nomor antrian yang akan di set
+	 */
 	void setQueue(Queue, int);
-	
+	/**
+	 * @brief getN
+	 * @details Menghitung banyaknya antrian yang dibuka
+	 * @return integer jumlah antrian yang dibuka
+	 */
 	int getN();
+	/**
+	 * @brief getDateTime
+	 * @details Mendapatkan DateTime dari objek Bank
+	 * @return DateTime dari objek bank akan menjadi kembalian
+	 */
 	DateTime getDateTime();
+	/**
+	 * @brief getQueue
+	 * @details Mendapatkan queue dengan urutan tertentu
+	 * 
+	 * @param  i urutan queue ke-i yang akan dijadikan kembalian
+	 * @return Queue ke-i
+	 */
 	Queue getQueue(int);
-		
+	/**
+	 * @brief Jockeying
+	 * @details Mengatur susunan antrian agar dapat menerapkan fungsi Jockeyin yakni tidak ada antrian yang memiliki selisih lebih dari 2
+	 * 
+	 * @param  i Pengecekan pada antrian ke-i
+	 * @return mengembalikan nilai -1 atau queue tujuan
+	 */
     int Jockeying(int);
+    /**
+     * @brief Arrival
+     * @details Menempatkan pengantri baru pada barisan antrian tertentu
+     * 
+     * @param  kode pengantri
+     */
     void Arrival(int);
+    /**
+     * @brief Departure
+     * @details Mengeluarkan pengantri dari barisan antrian
+     * 
+     * @param  kode pengantri yang akan dikeluarkan
+     */
     void Departure(int);
+    /**
+     * @brief Print
+     * @details Menampilkan seluruh antrian dari objek bank
+     */
 	void Print();
+	/**
+	 * @brief Sweep
+	 * @details Membersihkn antrian yang ada secara berurutan satu persatu tiap antrian
+	 */
 	void Sweep();
+	/**
+	 * @brief Push
+	 * @details Menambahkan pengantri pada barisan antrian tertentu
+	 * 
+	 * @param _nQ nomor antrian
+	 * @param _n nomor pengantri
+	 */
 	void Push(int _nQ,int _n);
 	static int i;
 
--- Queue.h
+++ Queue.h
@@ -9,7 +9,7 @@ class Queue
     public:
         /**
          * @brief Constructor
-         * @details Instansiasi objek Queue dnegan nilai awal
+         * @details Instansiasi objek Queue dengan nilai awal
          */
         Queue();
         /**
@@ -38,13 +38,53 @@ class Queue
          * @details Menghapus objek Queue dari memory
          */
         ~Queue();
+        /**
+         * @brief Fungsi isEmpty
+         * @details Mengecek apakah suatu objek Queue tidak memiliki elemen
+         * @return Boolean true atau false
+         */
         bool isEmpty();
+        /**
+         * @brief Fungsi isFull
+         * @details Mengecek apakah suatu ibjek Queue memiliki jumlah elemen maksimal dengan daya tampung
+         * @return boolean true atau false
+         */
         bool isFull();
+        /**
+         * @brief Methode push
+         * @details Menambahkan elemen baru pada tail objek Queue
+         * 
+         * @param  integer masukan dari pengguna
+         */
         void push(int);
+        /**
+         * @brief Methode pop_back
+         * @details Mengambil elemen terakhir dari suatu objek Queue dan memberikan nilai kembalian
+         * @return integer nilai elemen terakhir suatu objek Queue
+         */
         int pop_back();
+        /**
+         * @brief Methode pop_front
+         * @details Mengambil elemen pertama dari suatu objek Queue dan memberikan nilai kembalian
+         * @return integer nilai elemen pertama suatu objek Queue
+         */
 		int pop_front();
+        /**
+         * @brief Method size
+         * @details Mengetahui jumlah elemen dari suatu objek Queue
+         * @return integer jumlah elemen objek Queue
+         */
         int size();
+        /**
+         * @brief Methode Print
+         * @details Mencetak semua elemen objek Queue dengan formal [elemen1,elemen2,elemen3]
+         */
         void Print();
+        /**
+         * @brief Methode status
+         * @details Memberikan informasi apakah suatu objek Queue kosong
+         * @return boolean true atau false
+         */
         bool status();
     public:
         int* T;
